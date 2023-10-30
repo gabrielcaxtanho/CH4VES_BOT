20171a894d50b22b94fd62e2341d271152d5d074
Gabriel Castanho <96641560+gabrielcaxtanho@users.noreply.github.com>
2023-10-30 18:41:26 -0300

    Add files via upload

--- redesgpt.py
+++ redesgpt.py
@@ -1,9 +1,23 @@
 import wikipedia
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 app.secret_key = 'tiodino'
 wikipedia.set_lang('pt')
+
+
+# Lista para rastrear os acessos que foi criado na rota para verificar as pessoas que acessaram
+
+access_logs = []
+
+class AccessLog:
+    def __init__(self, ip_address, timestamp, location):
+        self.ip_address = ip_address
+        self.timestamp = timestamp
+        self.location = location
+
 
 @app.route('/resposta')
 def index():
@@ -31,10 +45,23 @@ def perguntass():
 def criar():
     pergunta = request.form['pergunta']
     session['pergunta'] = pergunta
+    ip_address = request.remote_addr
+    # Obter informações de geolocalização usando o serviço "ipinfo.io"
+    geo_info = requests.get(f"https://ipinfo.io/{ip_address}/json").json()
+    location = geo_info.get("city", "Desconhecida")  # Pode usar outros campos como "country" ou "region" conforme necessário
+
+    access_logs.append(AccessLog(ip_address, datetime.now(), location))
     if pergunta == '':
         return redirect('/')
     else:
         return redirect('/resposta')
+
+
+@app.route('/access_logs', methods=['GET'])
+def get_access_logs():
+    # Rota para visualizar os registros de acesso
+    logs = [{"ip_address": log.ip_address, "timestamp": log.timestamp.strftime("%Y-%m-%d %H:%M:%S"), "location": log.location} for log in access_logs]
+    return render_template('access_logs.html', logs=logs)
 
 if __name__ == "__main__":
     app.run(debug=True)
